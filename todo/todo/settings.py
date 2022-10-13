cb0f7a9d97f1bf505fcd11a19693fdd21d0129ec
shirguin <shirguin@yandex.ru>
2022-10-13 08:46:50 +0300

    Начал ДЗ к 6 уроку

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -148,6 +148,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 100
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 
 CORS_ALLOWED_ORIGINS = [
