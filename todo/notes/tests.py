a3a921bbf0a2007e67c60d2444eeb27ddaa8c857
shirguin <shirguin@yandex.ru>
2022-10-21 13:57:33 +0300

    ДЗ к 8 уроку

--- todo/notes/tests.py
+++ todo/notes/tests.py
@@ -27,10 +27,6 @@ class TestUserModelViewSet(TestCase):
         self.data_user_put = {'password': '12345', 'username': 'Petr', 'first_name': 'Петр', 'last_name': 'Петров',
                           'email': 'alex@mail.ru'}
 
-        self.data_project = {'name': 'Тестовый проект', 'link_repository': 'https://github.com/shirguin'}
-        self.data_project_put = {'name': 'Исправленный тестовый проект', 'link_repository': 'https://github.com/test'}
-        self.url_project = '/projects/'
-
 # APIRequestFactory
     def test_get_list(self):
         factory = APIRequestFactory()
@@ -88,6 +84,55 @@ class TestMath(APISimpleTestCase):
     def test_sqrt(self):
         self.assertEqual(math.sqrt(4), 2)
 
-# APITestCase
-# Остановился здесь!!!
 
+# APITestCase
+class TestToDo(APITestCase):
+    def setUp(self) -> None:
+        self.name = 'admin'
+        self.password = "12345"
+        self.email = 'admin@yandex.ru'
+        self.url = '/notes/'
+        self.admin = User.objects.create_superuser(self.name, self.email, self.password)
+        self.data_project = {'name': 'Тестовый проект', 'link_repository': 'https://github.com/'}
+        self.data_user = {'password': '12345', 'username': 'Alex', 'first_name': 'Алексей', 'last_name': 'Ширгин',
+                          'email': 'alex@mail.ru'}
+
+    def test_get_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_put_admin(self):
+        project = Project.objects.create(**self.data_project)
+        user = User.objects.create(**self.data_user)
+        todo = ToDo.objects.create(text='Тестовая заметка', project=project, user=user)
+        self.client.login(username=self.name, password=self.password)
+        response = self.client.put(f'{self.url}{todo.id}/', {'text': 'Измененная заметка', 'project': todo.project.id, 'user': user.id})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        todo_ = ToDo.objects.get(id=todo.id)
+        self.assertEqual(todo_.text, 'Измененная заметка')
+        self.client.logout()
+
+    def test_put_mixer(self):
+        user = User.objects.create(**self.data_user)
+        todo = mixer.blend(ToDo)
+        self.client.login(username=self.name, password=self.password)
+        response = self.client.put(f'{self.url}{todo.id}/', {'text': 'Измененная заметка', 'project': todo.project.id, 'user': user.id})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        todo_ = ToDo.objects.get(id=todo.id)
+        self.assertEqual(todo_.text, 'Измененная заметка')
+        self.client.logout()
+
+    def test_put_mixer_fields(self):
+        user = User.objects.create(**self.data_user)
+        todo = mixer.blend(ToDo, text='Заметка')
+        self.assertEqual(todo.text, 'Заметка')
+        self.client.login(username=self.name, password=self.password)
+        response = self.client.put(f'{self.url}{todo.id}/',
+                                   {'text': 'Измененная заметка', 'project': todo.project.id, 'user': user.id})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        todo_ = ToDo.objects.get(id=todo.id)
+        self.assertEqual(todo_.text, 'Измененная заметка')
+        self.client.logout()
